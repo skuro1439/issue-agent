b170eac5910a653b979c5e4a264de5e1195fa9c4
takeshi.koenuma <t.koenuma2@gmail.com>
2024-12-06 21:51:31 +0900

    refactor cloning repository process

---
+++ agent/functions/agithub/clone_repository.go
@@ -0,0 +1,31 @@
+package agithub
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github/clover0/github-issue-agent/config/cli"
+	"github/clover0/github-issue-agent/logger"
+)
+
+func CloneRepository(lo logger.Logger, cliIn cli.Inputs) error {
+	token, ok := os.LookupEnv("GITHUB_TOKEN")
+	if !ok {
+		lo.Error("GITHUB_TOKEN is not set")
+		return fmt.Errorf("GITHUB_TOKEN is not set")
+	}
+	lo.Info("cloning repository...\n")
+	cmd := exec.Command("git", "clone", "--depth", "1",
+		fmt.Sprintf("https://oauth2:%s@github.com/%s/%s.git", token, cliIn.RepositoryOwner, cliIn.Repository),
+		cliIn.AgentWorkDir,
+	)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		lo.Error(string(output))
+		return fmt.Errorf("failed to clone repository: %w", err)
+	}
+
+	lo.Info("cloned repository successfully")
+	return nil
+}
