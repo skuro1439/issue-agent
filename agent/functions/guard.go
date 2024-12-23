e54b7ee4c6769efdd4fca04e52a984fbcda78f2f
takeshi.koenuma <t.koenuma2@gmail.com>
2024-12-23 23:15:25 +0900

    enhance path securtiy

--- agent/functions/guard.go
+++ agent/functions/guard.go
@@ -2,6 +2,7 @@ package functions
 
 import (
 	"fmt"
+	"path/filepath"
 	"strings"
 )
 
@@ -20,6 +21,15 @@ func guardPath(path string) error {
 
 	if strings.HasPrefix(path, "/") {
 		return fmt.Errorf("path %s starts with '/', not allowed", path)
+	}
+
+	cleanPath := filepath.Clean(path)
+	if !filepath.IsLocal(cleanPath) {
+		return fmt.Errorf("path %s is not a local path", path)
+	}
+
+	if strings.HasPrefix(cleanPath, "..") {
+		return fmt.Errorf("path %s attempts to access parent directory", path)
 	}
 
 	return nil
