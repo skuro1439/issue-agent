4c7d44fb4bf6fa01abd9ddaf0c7e8c548398177a
takeshi.koenuma <t.koenuma2@gmail.com>
2025-01-17 21:07:33 +0900

    update flag description

--- agent/cli/common_input.go
+++ agent/cli/common_input.go
@@ -13,7 +13,7 @@ type CommonInput struct {
 
 func addCommonFlags(fs *flag.FlagSet, cfg *CommonInput) {
 	fs.StringVar(&cfg.Config, "config", "", "Path to the configuration file. Default is `agent/config/default_config.yml in this project`")
-	fs.StringVar(&cfg.Language, "language", "", "Language spoken agent")
-	fs.StringVar(&cfg.Model, "model", "", "LLM Model name")
-	fs.StringVar(&cfg.GitHubOwner, "github_owner", "", "GitHub owner of the repository")
+	fs.StringVar(&cfg.Language, "language", "", "Language spoken by agent. Default is English")
+	fs.StringVar(&cfg.Model, "model", "", "LLM Model name. Default is `claude-3-5-sonnet-latest`")
+	fs.StringVar(&cfg.GitHubOwner, "github_owner", "", "The GitHub account owner of the repository. Required")
 }
