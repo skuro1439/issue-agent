9272339351f5a6c42898e60f3c76b77fe3fdba46
takeshi.koenuma <t.koenuma2@gmail.com>
2025-01-25 19:43:43 +0900

    add assertion for struct pointer comparison

--- agent/test/assert/assert.go
+++ agent/test/assert/assert.go
@@ -1,9 +1,30 @@
 package assert
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func Equal[V comparable](t *testing.T, got, expected V) {
 	t.Helper()
+
+	gotIsPtr := reflect.TypeOf(got).Kind() == reflect.Pointer
+	expectedIsPtr := reflect.TypeOf(expected).Kind() == reflect.Pointer
+
+	// compare pointers
+	if gotIsPtr && expectedIsPtr {
+		fmt.Println("both are pointers")
+		t.Log("both are pointers")
+		fmt.Printf("got: %v\n", reflect.ValueOf(got).Elem())
+		if !reflect.ValueOf(got).Elem().Equal(reflect.ValueOf(expected).Elem()) {
+			t.Errorf(`assert.Equal(
+got: %v
+expected: %v
+)`, got, expected)
+		}
+		return
+	}
 
 	if expected != got {
 		t.Errorf(`assert.Equal(
@@ -21,10 +42,29 @@ func Nil(t *testing.T, value any) {
 	}
 }
 
+func EqualStringSlices(t *testing.T, got, expected []string) {
+	t.Helper()
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf(`assert.EqualStringSlices(
+got: %v
+expected: %v
+)`, got, expected)
+	}
+}
+
 func HasError(t *testing.T, err error) {
 	t.Helper()
 
 	if err == nil {
 		t.Errorf(`expected error, got nil`)
+	}
+}
+
+func NoError(t *testing.T, err error) {
+	t.Helper()
+
+	if err != nil {
+		t.Errorf(`expected no error, got %v`, err)
 	}
 }
